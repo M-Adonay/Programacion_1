0973115d77aa438924df0316cbbf10bc82cfb490
Mauricio Adonay Espinoza Villatoro <usis018120@ugb.edu.sv>
2020-08-03 01:17:32 -0600

    Agregar archivos de proyecto.

---
+++ HolaMundo/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btncalcular = New System.Windows.Forms.Button()
+        Me.cboOperaciones = New System.Windows.Forms.ComboBox()
+        Me.lblnum1 = New System.Windows.Forms.Label()
+        Me.lblnum2 = New System.Windows.Forms.Label()
+        Me.lblrespuesta = New System.Windows.Forms.Label()
+        Me.txtnum2 = New System.Windows.Forms.TextBox()
+        Me.txtnum1 = New System.Windows.Forms.TextBox()
+        Me.optSuma = New System.Windows.Forms.RadioButton()
+        Me.optMultiplicar = New System.Windows.Forms.RadioButton()
+        Me.optResta = New System.Windows.Forms.RadioButton()
+        Me.optPorcentaje = New System.Windows.Forms.RadioButton()
+        Me.optDividir = New System.Windows.Forms.RadioButton()
+        Me.optResiduo = New System.Windows.Forms.RadioButton()
+        Me.optExponente = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'btncalcular
+        '
+        Me.btncalcular.Location = New System.Drawing.Point(168, 222)
+        Me.btncalcular.Name = "btncalcular"
+        Me.btncalcular.Size = New System.Drawing.Size(134, 46)
+        Me.btncalcular.TabIndex = 0
+        Me.btncalcular.Text = "Calcular"
+        Me.btncalcular.UseVisualStyleBackColor = True
+        '
+        'cboOperaciones
+        '
+        Me.cboOperaciones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboOperaciones.FormattingEnabled = True
+        Me.cboOperaciones.Items.AddRange(New Object() {"Seleccione una opcion", "Suma", "Resta", "Multiplicacion", "Division", "Porcentaje", "Exponente", "Residuo"})
+        Me.cboOperaciones.Location = New System.Drawing.Point(345, 132)
+        Me.cboOperaciones.Name = "cboOperaciones"
+        Me.cboOperaciones.Size = New System.Drawing.Size(121, 21)
+        Me.cboOperaciones.TabIndex = 1
+        '
+        'lblnum1
+        '
+        Me.lblnum1.AutoSize = True
+        Me.lblnum1.Location = New System.Drawing.Point(11, 40)
+        Me.lblnum1.Name = "lblnum1"
+        Me.lblnum1.Size = New System.Drawing.Size(35, 13)
+        Me.lblnum1.TabIndex = 2
+        Me.lblnum1.Text = "Num1"
+        '
+        'lblnum2
+        '
+        Me.lblnum2.AutoSize = True
+        Me.lblnum2.Location = New System.Drawing.Point(230, 40)
+        Me.lblnum2.Name = "lblnum2"
+        Me.lblnum2.Size = New System.Drawing.Size(35, 13)
+        Me.lblnum2.TabIndex = 3
+        Me.lblnum2.Text = "Num2"
+        '
+        'lblrespuesta
+        '
+        Me.lblrespuesta.AutoSize = True
+        Me.lblrespuesta.Location = New System.Drawing.Point(396, 37)
+        Me.lblrespuesta.Name = "lblrespuesta"
+        Me.lblrespuesta.Size = New System.Drawing.Size(70, 13)
+        Me.lblrespuesta.TabIndex = 4
+        Me.lblrespuesta.Text = "Respuesta: ?"
+        '
+        'txtnum2
+        '
+        Me.txtnum2.Location = New System.Drawing.Point(271, 37)
+        Me.txtnum2.Name = "txtnum2"
+        Me.txtnum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtnum2.TabIndex = 6
+        '
+        'txtnum1
+        '
+        Me.txtnum1.Location = New System.Drawing.Point(52, 37)
+        Me.txtnum1.Name = "txtnum1"
+        Me.txtnum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtnum1.TabIndex = 7
+        '
+        'optSuma
+        '
+        Me.optSuma.AutoSize = True
+        Me.optSuma.Location = New System.Drawing.Point(169, 40)
+        Me.optSuma.Name = "optSuma"
+        Me.optSuma.Size = New System.Drawing.Size(31, 17)
+        Me.optSuma.TabIndex = 8
+        Me.optSuma.TabStop = True
+        Me.optSuma.Text = "+"
+        Me.optSuma.UseVisualStyleBackColor = True
+        '
+        'optMultiplicar
+        '
+        Me.optMultiplicar.AutoSize = True
+        Me.optMultiplicar.Location = New System.Drawing.Point(168, 86)
+        Me.optMultiplicar.Name = "optMultiplicar"
+        Me.optMultiplicar.Size = New System.Drawing.Size(29, 17)
+        Me.optMultiplicar.TabIndex = 9
+        Me.optMultiplicar.TabStop = True
+        Me.optMultiplicar.Text = "*"
+        Me.optMultiplicar.UseVisualStyleBackColor = True
+        '
+        'optResta
+        '
+        Me.optResta.AutoSize = True
+        Me.optResta.Location = New System.Drawing.Point(169, 63)
+        Me.optResta.Name = "optResta"
+        Me.optResta.Size = New System.Drawing.Size(28, 17)
+        Me.optResta.TabIndex = 10
+        Me.optResta.TabStop = True
+        Me.optResta.Text = "-"
+        Me.optResta.UseVisualStyleBackColor = True
+        '
+        'optPorcentaje
+        '
+        Me.optPorcentaje.AutoSize = True
+        Me.optPorcentaje.Location = New System.Drawing.Point(168, 132)
+        Me.optPorcentaje.Name = "optPorcentaje"
+        Me.optPorcentaje.Size = New System.Drawing.Size(76, 17)
+        Me.optPorcentaje.TabIndex = 11
+        Me.optPorcentaje.TabStop = True
+        Me.optPorcentaje.Text = "Porcentaje"
+        Me.optPorcentaje.UseVisualStyleBackColor = True
+        '
+        'optDividir
+        '
+        Me.optDividir.AutoSize = True
+        Me.optDividir.Location = New System.Drawing.Point(168, 109)
+        Me.optDividir.Name = "optDividir"
+        Me.optDividir.Size = New System.Drawing.Size(30, 17)
+        Me.optDividir.TabIndex = 12
+        Me.optDividir.TabStop = True
+        Me.optDividir.Text = "/"
+        Me.optDividir.UseVisualStyleBackColor = True
+        '
+        'optResiduo
+        '
+        Me.optResiduo.AutoSize = True
+        Me.optResiduo.Location = New System.Drawing.Point(167, 178)
+        Me.optResiduo.Name = "optResiduo"
+        Me.optResiduo.Size = New System.Drawing.Size(64, 17)
+        Me.optResiduo.TabIndex = 13
+        Me.optResiduo.TabStop = True
+        Me.optResiduo.Text = "Residuo"
+        Me.optResiduo.UseVisualStyleBackColor = True
+        '
+        'optExponente
+        '
+        Me.optExponente.AutoSize = True
+        Me.optExponente.Location = New System.Drawing.Point(167, 155)
+        Me.optExponente.Name = "optExponente"
+        Me.optExponente.Size = New System.Drawing.Size(31, 17)
+        Me.optExponente.TabIndex = 14
+        Me.optExponente.TabStop = True
+        Me.optExponente.Text = "^"
+        Me.optExponente.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(473, 326)
+        Me.Controls.Add(Me.optExponente)
+        Me.Controls.Add(Me.optResiduo)
+        Me.Controls.Add(Me.optDividir)
+        Me.Controls.Add(Me.optPorcentaje)
+        Me.Controls.Add(Me.optResta)
+        Me.Controls.Add(Me.optMultiplicar)
+        Me.Controls.Add(Me.optSuma)
+        Me.Controls.Add(Me.txtnum1)
+        Me.Controls.Add(Me.txtnum2)
+        Me.Controls.Add(Me.lblrespuesta)
+        Me.Controls.Add(Me.lblnum2)
+        Me.Controls.Add(Me.lblnum1)
+        Me.Controls.Add(Me.cboOperaciones)
+        Me.Controls.Add(Me.btncalcular)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Calculadora Basica"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btncalcular As Button
+    Friend WithEvents cboOperaciones As ComboBox
+    Friend WithEvents lblnum1 As Label
+    Friend WithEvents lblnum2 As Label
+    Friend WithEvents lblrespuesta As Label
+    Friend WithEvents txtnum2 As TextBox
+    Friend WithEvents txtnum1 As TextBox
+    Friend WithEvents optSuma As RadioButton
+    Friend WithEvents optMultiplicar As RadioButton
+    Friend WithEvents optResta As RadioButton
+    Friend WithEvents optPorcentaje As RadioButton
+    Friend WithEvents optDividir As RadioButton
+    Friend WithEvents optResiduo As RadioButton
+    Friend WithEvents optExponente As RadioButton
+End Class
---
+++ HolaMundo/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Private Sub CalculadoraBasica_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblnum1.Click
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btncalcular_Click(sender As Object, e As EventArgs) Handles btncalcular.Click
+        Dim num1, num2, respuesta As Double
+
+        num1 = txtnum1.Text
+        num2 = txtnum2.Text
+
+        If optSuma.Checked Then
+            lblrespuesta.Text = num1 + num2
+        End If
+
+        If optResta.Checked Then
+            lblrespuesta.Text = num1 - num2
+        End If
+
+        If optMultiplicar.Checked Then
+            lblrespuesta.Text = num1 * num2
+        End If
+
+        If optDividir.Checked Then
+            lblrespuesta.Text = num1 / num2
+        End If
+
+        If optPorcentaje.Checked Then
+            lblrespuesta.Text = num1 * num2 / 100
+        End If
+
+        If optExponente.Checked Then
+            lblrespuesta.Text = num1 ^ num2
+        End If
+
+        If optResiduo.Checked Then
+            lblrespuesta.Text = num1 Mod num2
+        End If
+
+        Select Case cboOperaciones.SelectedIndex
+            Case 1
+                lblrespuesta.Text = num1 + num2
+
+            Case 2
+                lblrespuesta.Text = num1 - num2
+
+            Case 3
+                lblrespuesta.Text = num1 * num2
+
+            Case 4
+                lblrespuesta.Text = num1 / num2
+
+            Case 5
+                lblrespuesta.Text = num1 * num2 / 100
+
+            Case 6
+                lblrespuesta.Text = num1 ^ num2
+
+            Case 7
+                lblrespuesta.Text = num1 Mod num2
+        End Select
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles optMultiplicar.CheckedChanged
+
+    End Sub
+End Class
