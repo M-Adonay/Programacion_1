3ff6bf5b5001c05632fd6a77695210a709a52428
Mauricio Adonay Espinoza Villatoro <usis018120@ugb.edu.sv>
2020-08-14 16:54:23 -0600

    se elimino el limite de tablas hasta 12 y se implemento el ciclo Do While

--- HolaMundo/Form1.vb
+++ HolaMundo/Form1.vb
@@ -20,18 +20,16 @@
     End Sub
 
     Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
-        Dim ntabla, i As SByte
+        Dim ntabla, i As Int64
         ntabla = txtntabla.Text
+        i = 1
 
         lstTablas.Items.Clear()
 
-        If ntabla <= 12 Then
-            For i = 1 To 10
-                lstTablas.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
+        Do While i <= 10
+            lstTablas.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
+            i = i + 1
+        Loop
 
-            Next
-        Else
-            lstTablas.Items.Add("Solo tablas menores o iguales a 12.")
-        End If
     End Sub
 End Class
